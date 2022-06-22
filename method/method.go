1d17e6c726bbfbd6bccf15101b05ad96f536ba08
FedericoM-DoIT <federico.mollo@doit-systems.it>
2022-06-22 18:20:54 +0200

    Added io.MultiWriter (to Stdout and buffer)

--- method/method.go
+++ method/method.go
@@ -83,8 +83,8 @@ func Shellout(command string) (error, string, string) {
     var stdout bytes.Buffer
     var stderr bytes.Buffer
     cmd := exec.Command(ShellToUse, "-c", command)
-    cmd.Stdout = &stdout
-    cmd.Stderr = &stderr
+    cmd.Stdout = io.MultiWriter(os.Stdout, &stdout)
+    cmd.Stderr = io.MultiWriter(os.Stderr, &stderr)
     err := cmd.Run()
     return err, stdout.String(), stderr.String()
 }
